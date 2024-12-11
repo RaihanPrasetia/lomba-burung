906f0a7bf6dde6f419e9f295434db55d5161e117
khairullah <khairullahzikri212@gmail.com>
2024-12-12 01:48:37 +0700

    Add Label

--- resources/views/components/modals/modalAdd.blade.php
+++ resources/views/components/modals/modalAdd.blade.php
@@ -17,28 +17,20 @@
                 <form action="#">
                     <div class="grid grid-cols-6 gap-6">
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="first-name" class="block mb-2 text-sm font-medium text-gray-900">First Name</label>
-                            <input type="text" name="first-name" id="first-name" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="Bonnie" required>
-                        </div>
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="last-name" class="block mb-2 text-sm font-medium text-gray-900">Last Name</label>
-                            <input type="text" name="last-name" id="last-name" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="Green" required>
+                            <label for="first-name" class="block mb-2 text-sm font-medium text-gray-900">Nama Lengkap</label>
+                            <input type="text" name="first-name" id="first-name" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="Nama Lengkap" required>
                         </div>
                         <div class="col-span-6 sm:col-span-3">
                             <label for="email" class="block mb-2 text-sm font-medium text-gray-900">Email</label>
                             <input type="email" name="email" id="email" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="example@company.com" required>
                         </div>
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="phone-number" class="block mb-2 text-sm font-medium text-gray-900">Phone Number</label>
-                            <input type="number" name="phone-number" id="phone-number" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="e.g. +(12)3456 789" required>
+                            <label for="last-name" class="block mb-2 text-sm font-medium text-gray-900">Jenis Kelamin</label>
+                            <input type="text" name="last-name" id="last-name" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="Jenis Kelamin" required>
                         </div>
                         <div class="col-span-6 sm:col-span-3">
-                            <label for="department" class="block mb-2 text-sm font-medium text-gray-900">Department</label>
-                            <input type="text" name="department" id="department" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="Development" required>
-                        </div>
-                        <div class="col-span-6 sm:col-span-3">
-                            <label for="company" class="block mb-2 text-sm font-medium text-gray-900">Company</label>
-                            <input type="number" name="company" id="company" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="123456" required>
+                            <label for="department" class="block mb-2 text-sm font-medium text-gray-900">Status</label>
+                            <input type="text" name="department" id="department" class="shadow-lg-sm border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-2 focus:ring-fuchsia-50 focus:border-fuchsia-300 block w-full p-2.5" placeholder="Status" required>
                         </div>
                     </div> 
                 </div>
