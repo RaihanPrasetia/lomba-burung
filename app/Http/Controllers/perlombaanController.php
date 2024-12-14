2fe6a95a51b35915b52c708bf766e85e6d11aaab
khairullah <khairullahzikri212@gmail.com>
2024-12-14 10:27:22 +0700

    Use Illuminate

--- app/Http/Controllers/perlombaanController.php
+++ app/Http/Controllers/perlombaanController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Competition;
 use Illuminate\Http\Request;
 
 class PerlombaanController extends Controller
--- app/Models/Class_Participants.php
+++ app/Models/Class_Participants.php
@@ -2,6 +2,7 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Class_Participants extends Model
